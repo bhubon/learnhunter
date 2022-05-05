12acc570ba771414304d6de9294eac188f9fc941
bhubon-nil <sdbhubon@gmail.com>
2022-05-05 14:06:53 +0600

    laravel service provider

--- app/Providers/ExampleProvider.php
+++ app/Providers/ExampleProvider.php
@@ -4,24 +4,29 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class ExampleProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register services.
      *
      * @return void
      */
     public function register()
     {
+        //
     }
 
     /**
-     * Bootstrap any application services.
+     * Bootstrap services.
      *
      * @return void
      */
     public function boot()
     {
-        //
+        $data=array();
+        $data['a']=10;
+        $data['b']=20;
+        $data['c']=30;
+        view()->share('number',$data);
     }
 }
--- routes/amar.php
+++ routes/amar.php
@@ -13,15 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/amar', function () {
+    return view('amar');
 });
-// Route::get('/test', function () {
-//     app()->make('first_service_helper');
-// });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
